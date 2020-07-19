d8fdd06083c3213a6239224a04e2642e31bbfdaa
Srijanathulung <srijana.thulung123@gmail.com>
2020-07-19 18:58:57 +0545

    bouncing ball incomplete

--- Javascript-Assignments/assignment4/main.js
+++ Javascript-Assignments/assignment4/main.js
@@ -1,21 +1,37 @@
 var btn = document.getElementById("btn");
+var i = 0;
+var direction = 1;
 
 btn.addEventListener("click", function () {
   var ball = document.createElement("div");
   var box = document.getElementById("box1");
 
+  ball.id = i;
   ball.style.height = "25px";
   ball.style.width = "25px";
   ball.style.backgroundColor = "red";
   ball.style.borderRadius = "50%";
 
-  box.style.height = "300px";
-  box.style.width = "300px";
-  box.style.border = "2px solid black";
-  box.style.position = "relative";
+  box.style.position = "absolute";
 
   ball.style.position = "absolute";
   ball.style.top = Math.random() * (box.clientHeight - 25) + "px";
-  ball.style.left = Math.random() * (box.Width - 25) + "px";
+  ball.style.left = Math.random() * (box.clientWidth - 25) + "px";
+
+  ball.addEventListener("click", function (e) {
+    //move continously
+    setInterval(function () {
+      var currentTop = ball.style.top;
+      var newValue = parseInt(currentTop) + 10 * direction;
+      ball.style.top = newValue + "px";
+
+      var clientHeight = box.clientHeight;
+
+      if (newValue >= clientHeight - 25 || newValue - 25 <= 0) {
+        direction *= -1;
+      }
+    }, 1000 / 60);
+  });
   box.appendChild(ball);
+  i++;
 });
