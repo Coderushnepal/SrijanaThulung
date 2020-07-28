682c83d7b0c622880a3ba08c66ab5fac7f4b0633
Srijanathulung <srijana.thulung123@gmail.com>
2020-07-28 20:58:29 +0545

    hanggame incomplete

--- Javascript-Assignments/assignment8/hangman.js
+++ Javascript-Assignments/assignment8/hangman.js
@@ -17,6 +17,24 @@ subHeader.classList.add("sub-header");
 subHeader.innerHTML = "Find the hidden word- Enter a letter";
 header.appendChild(subHeader);
 
+//for win result
+var winResult = document.createElement("div");
+winResult.id = "winBox";
+container.appendChild(winResult);
+
+var winValue = document.createElement("div");
+winValue.classList.add("text");
+winValue.innerHTML = "Congratulation,YOU WIN!";
+winResult.appendChild(winValue);
+
+//for loss result
+var lossResult = document.createElement("div");
+lossResult.id = "lossBox";
+container.appendChild(lossResult);
+var lossValue = document.createElement("div");
+lossValue.classList.add("text");
+lossValue.innerHTML = "SORRY,YOU LOST!!";
+lossResult.appendChild(lossValue);
 //WRONGwORDS
 var wrongDiv = document.createElement("div");
 wrongDiv.classList.add("wrong-div");
@@ -50,7 +68,7 @@ var fruits = [
 
 var fruitList = fruits[Math.floor(Math.random() * fruits.length)];
 console.log(fruitList);
-
+var correctlyGuessedLetters = [];
 for (var i = 0; i < fruitList.length; i++) {
   var letterDash = document.createElement("div");
   letterDash.classList.add("dashes");
@@ -63,12 +81,35 @@ wrongWords.appendChild(wrongGuess);
 
 var dashBox = document.getElementsByClassName("dashes");
 
+//toast-message
+var toast = document.createElement("div");
+toast.classList.add("message");
+container.appendChild(toast);
+var notification = document.createElement("h3");
+notification.innerHTML = "Already guessed";
+
+toast.appendChild(notification);
+
 document.addEventListener("keydown", function (e) {
   console.log(e);
   if (fruitList.includes(e.key)) {
+    if (correctlyGuessedLetters.includes(e.key)) {
+      // console.log("already guessed ", e.key);
+      toast.style.display = "inline-block";
+      setTimeout(() => {
+        toast.style.display = "none";
+      }, 1000);
+    }
     for (var j = 0; j < fruitList.length; j++) {
       if (fruitList[j] === e.key) {
+        correctlyGuessedLetters.push(e.key);
         dashBox[j].innerHTML = e.key;
+
+        if (correctlyGuessedLetters.length === fruitList.length) {
+          var winBox = this.getElementById("winBox");
+          console.log("you win");
+          winBox.style.display = "inline-block";
+        }
       }
     }
   } else {
@@ -78,6 +119,16 @@ document.addEventListener("keydown", function (e) {
     // console.log(wrongGuess);
     if (!wrongLetters.includes(e.key)) {
       wrongLetters.push(e.key);
+    } else {
+      toast.style.display = "inline-block";
+      setTimeout(() => {
+        toast.style.display = "none";
+      }, 1000);
+    }
+    if (wrongLetters.length === 6) {
+      console.log("you lose");
+      var lossBox = this.getElementById("lossBox");
+      lossBox.style.display = "inline-block";
     }
     wrongGuess.innerHTML = wrongLetters.join(",");
     console.log(wrongLetters);
