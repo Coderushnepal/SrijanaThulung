a518d3ea49a7d01b9c3de170fe3b3f4d98411aac
Srijanathulung <srijana.thulung123@gmail.com>
2020-07-30 17:32:46 +0545

    Hangman complete

--- Javascript-Assignments/assignment8/hangman.js
+++ Javascript-Assignments/assignment8/hangman.js
@@ -1,12 +1,8 @@
-var container = document.createElement("div");
-container.classList.add("container");
-document.body.appendChild(container);
-
 //HEADER
 var header = document.createElement("h1");
 header.classList.add("heading");
 header.innerHTML = "Hangman";
-container.appendChild(header);
+document.body.appendChild(header);
 
 var breakline = document.createElement("br");
 header.appendChild(breakline);
@@ -17,68 +13,10 @@ subHeader.classList.add("sub-header");
 subHeader.innerHTML = "Find the hidden word- Enter a letter";
 header.appendChild(subHeader);
 
-//for win result
-var winResult = document.createElement("div");
-winResult.id = "winBox";
-container.appendChild(winResult);
-
-var winValue = document.createElement("div");
-winValue.classList.add("text");
-winValue.innerHTML = "Congratulation,YOU WIN!";
-winResult.appendChild(winValue);
-
-//for win--button
-var winButton = document.createElement("button");
-winButton.innerHTML = "Play again";
-winButton.id = "winBtn";
-winButton.onclick = "win()";
-console.log(winResult);
-winResult.appendChild(winButton);
-
-winButton.addEventListener("click", () => {
-  console.log("123");
-});
-
-// // // for win button
-// // winButton.onclick = function win() {
-// //   window.location.reload();
-// // };
-// function win() {
-//   console.log("apple");
-//   window.location.reload();
-// }
-// winButton.onclick = win()
-// var btn1 = document.getElementById("winBtn");
-// btn1.addEventListener("click", function (e) {
-
-//   window.location.reload(true);
-// });
-// console.log(winButton);
-//for loss result
-var lossResult = document.createElement("div");
-lossResult.id = "lossBox";
-container.appendChild(lossResult);
-var lossValue = document.createElement("div");
-lossValue.classList.add("text");
-lossValue.innerHTML = "SORRY,YOU LOST!!";
-lossResult.appendChild(lossValue);
-
-//for loss--button
-var lossButton = document.createElement("button");
-lossButton.innerHTML = "Play again";
-lossButton.id = "lossBtn";
-lossResult.appendChild(lossButton);
-
-// // for loss button
-// lossButton.addEventListener("click", function (e) {
-//   window.location.reload();
-//   return false;
-// });
-
 //WRONGwORDS
 var wrongDiv = document.createElement("div");
 wrongDiv.classList.add("wrong-div");
-container.appendChild(wrongDiv);
+document.body.appendChild(wrongDiv);
 var wrongWords = document.createElement("div");
 wrongWords.classList.add("wrong");
 wrongWords.innerHTML = "Wrong words";
@@ -89,12 +27,12 @@ wrongWords.appendChild(breakline);
 //DASHDIV
 var dashDiv = document.createElement("div");
 dashDiv.classList.add("dash-div");
-container.appendChild(dashDiv);
+document.body.appendChild(dashDiv);
 
 //HINTS
 var hint = document.createElement("h2");
 hint.innerHTML = "Hint: types of fruits";
-container.appendChild(hint);
+document.body.appendChild(hint);
 
 var fruits = [
   "apple",
@@ -124,7 +62,7 @@ var dashBox = document.getElementsByClassName("dashes");
 //toast-message
 var toast = document.createElement("div");
 toast.classList.add("message");
-container.appendChild(toast);
+document.body.appendChild(toast);
 var notification = document.createElement("h3");
 notification.innerHTML = "Already guessed";
 
@@ -146,7 +84,7 @@ document.addEventListener("keydown", function (e) {
         dashBox[j].innerHTML = e.key;
 
         if (correctlyGuessedLetters.length === fruitList.length) {
-          var winBox = this.getElementById("winBox");
+          var winBox = document.getElementById("winBox");
           console.log("you win");
           winBox.style.display = "block";
         }
@@ -167,15 +105,10 @@ document.addEventListener("keydown", function (e) {
     }
     if (wrongLetters.length === 6) {
       console.log("you lose");
-      var lossBox = this.getElementById("lossBox");
+      var lossBox = document.getElementById("lossBox");
       lossBox.style.display = "inline-block";
     }
     wrongGuess.innerHTML = wrongLetters.join(",");
     console.log(wrongLetters);
   }
 });
-
-// // for loss button
-// lossButton.addEventListener("click", function (e) {
-//   window.location.reload();
-// });
